f790173cd057d2417daa559401b2038a093a62b8
Ductapemaster <Ductapemaster@gmail.com>
2014-08-12 10:47:19 -0700

    Removed rummy-specific code from card and Card_tests.  Added appropriate wildcards and handling to both files.

--- Card_tests.py
+++ Card_tests.py
@@ -10,56 +10,42 @@ class CardTests(unittest.TestCase):
 	def test_constructor_works_as_expected(self):
 		rank = 3
 		suit = 2
-		c = Card(rank, suit)
+		c = WizardCard(rank, suit)
 		self.assertEqual(c.rank, rank)
-		self.assertEqual(c.alt_rank, rank)
 		self.assertEqual(c.suit, suit)
-	
+
 	def test_constructor_with_face_card(self):
 		rank = 12
 		suit = 3
-		c = Card(rank, suit)
+		c = WizardCard(rank, suit)
 		self.assertEqual(c.rank, rank)
-		self.assertEqual(c.alt_rank, rank)
 		self.assertEqual(c.suit, suit)
-		
+
 	def test_constructor_with_ace(self):
-		rank = 1
+		rank = 14
 		suit = 2
-		c = Card(rank, suit)
+		c = WizardCard(rank, suit)
 		self.assertEqual(c.rank, rank)
-		self.assertEqual(c.alt_rank, 14)
+		self.assertEqual(c.suit, suit)
+
+	def test_constructor_with_Jester(self):
+		rank = 1
+		suit = 0
+		c = WizardCard(rank, suit)
+		self.assertEqual(c.rank, rank)
+		self.assertEqual(c.suit, suit)
+
+	def test_constructor_with_wizard(self):
+		rank = 15
+		suit = 0
+		c = WizardCard(rank, suit)
+		self.assertEqual(c.rank, rank)
 		self.assertEqual(c.suit, suit)
 
 	def test_constructor_with_null_card(self):
-		c = Card()
+		c = WizardCard()
 		self.assertEqual(c.rank, -1)
-		self.assertEqual(c.alt_rank, -1)
 		self.assertEqual(c.suit, -1)
-
-	def test_numPoints_returns_5_for_regular_cards(self):
-		for r in range(2,10):
-			for s in range(1,5):
-				c = Card(r, s)
-				self.assertEqual(c.numPoints(), 5)
-
-	def test_numPoints_returns_10_for_face_and_10_cards(self):
-		for r in range(10,14):
-			for s in range(1,5):
-				c = Card(r, s)
-				self.assertEqual(c.numPoints(), 10)
-
-	def test_numPoints_returns_15_for_ace_cards(self):
-		r = 1
-		for s in range(1,5):
-			c = Card(r, s)
-			self.assertEqual(c.numPoints(), 15)
-
-	def test_numPoints_returns_15_for_ace_cards(self):
-		r = 20
-		for s in range(1,5):
-			c = Card(r, s)
-			self.assertEqual(c.numPoints(), 20)
 
 if __name__ == '__main__':
 	unittest.main()
--- card.py
+++ card.py
@@ -1,45 +1,31 @@
-#null card is rank = 0, suit = 0
+# null card is rank = 0, suit = 0
+# Jester is rank = 1
+# Wizard is rank = 15
 
-class Card:
+class WizardCard:
 
 	# Define face cards
-	face_cards = {-1: 'NL', 1:'AL', 10: '10', 11:'J ', 12:'Q ', 13:'K ', 14:'AH', 20:'JK'}
-	# Define suits, 0 is a null suit
-	suits = {-1: 'N', 1: 'S', 2: 'H', 3: 'C', 4: 'D'}
+	face_cards = {-1: 'NUL', 1: 'JES', 10: '10 ', 11:'JAK', 12:'QEN', 13:'KNG', 14:'ACE', 15:'WIZ'}
+	# Define suits, 0 is a wildcard suit (represents all suits)
+	suits = {-1: 'N', 0: 'WILD', 1: 'S', 2: 'H', 3: 'C', 4: 'D'}
 
 	def __init__(self, rank=-1, suit=-1):
 		self.rank = rank
-		self.suit = suit
-		self.alt_rank = rank
-		if rank == 1:
-			self.alt_rank = 14
+		if rank == 1 or rank == 15:
+			self.suit = 0
+		else:
+			self.suit = suit
 		
 	def __repr__(self, debug=False):
 		# Return face card rank if in dict, otherwise return string of the rank (2-9)
-		readable_rank = self.face_cards.get(self.rank, str(self.rank) + ' ')
-		readable_alt_rank = self.face_cards.get(self.alt_rank, str(self.alt_rank) + ' ')
+		readable_rank = self.face_cards.get(self.rank, str(self.rank) + '  ')
+		#readable_alt_rank = self.face_cards.get(self.alt_rank, str(self.alt_rank) + ' ')
 		readable_suit = self.suits.get(self.suit)
-		if(debug): 
-			return "<%s %s %s>" % (readable_rank, readable_alt_rank, readable_suit)
-		else:
-			return "<%s %s>" % (readable_rank, readable_suit)
+
+		return "<%s %s>" % (readable_rank, readable_suit)
 		
 	def __eq__(self, other):
 		if isinstance(other, self.__class__):
 			return self.suit == other.suit and self.rank == other.rank
 		else:
 			return False
-
-
-	def numPoints(self):
-		if self.rank > 9 and self.rank < 14:
-			return 10
-		elif self.rank == 1:
-			return 15
-		elif self.rank == 20:
-			return 20
-		elif self.rank == -1:
-			return 0
-		else:
-			return 5
-
